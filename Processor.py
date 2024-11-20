15a3a646b006659945e15b28763d7a14cada42ec
SuperQKi <146518675+SuperQKi@users.noreply.github.com>
2024-11-21 00:01:37 +0700

    Update Processor.py

--- Processor.py
+++ Processor.py
@@ -1,12 +1,19 @@
 from registerFiles import registerfiles
 from register_table import register_table
 from machineCode_parser import machineCode_parser
+import sys
+try:
+    print('Pprogram running...')
 
+    file_in = open('Machine_code.txt')
+    instructions = []
+    for line in file_in:
+        line = line.strip()
+        instructions.append(line)
+    parse_instructions = machineCode_parser(registerfiles,register_table)
+    parse_instructions.parse(instructions)
+    sys.exit(0)
 
-file_in = open('Machine_code.txt')
-instructions = []
-for line in file_in:
-    line = line.strip()
-    instructions.append(line)
-parse_instructions = machineCode_parser(registerfiles,register_table)
-parse_instructions.parse(instructions)
+except Exception as e:
+    print(f'Program failed: {e}')
+    sys.exit(1)
